ac3f8cf26a538c4cea5e101354fa0a1940381855
H-Yu1999 <68189606+H-Yu1999@users.noreply.github.com>
2021-12-23 12:09:41 -0500

    Add files via upload

---
+++ Final_project/CallPlan.m
@@ -0,0 +1,31 @@
+clear
+close all
+RRTStar;
+detailedPath;
+
+initialSim2;
+figure
+axis([0 3 -3 3,0,6])
+set(gcf,'position',[0,0,1024,1024])
+%axis([0 4 -1 2,0,6])
+%filename = 'testAnimated1.gif';
+%hold on
+out = sim('FinalPlan');
+%hold off
+% figure 
+% plot(step:step:step*size(out.estimate(:,1),1),out.estimate(:,1))
+% hold on
+% plot(step:step:step*size(out.estimate(:,1),1),out.estimate(:,2))
+% 
+% plot(step:step:step*size(out.estimate(:,1),1),out.estimate(:,3))
+% title('\psi angle')
+% legend('\psi angle based on observation','\psi angle based on kalman filter','real \psi angle')
+% figure
+% %axis('tight')
+%% 3D figure
+figure
+for k = 1:6:size(out.simout)
+    detectTime(X,Y,Z,out.simout(k,1),out.simout(k,2),out.simout(k,5),out.simout(k,3),out.simout(k,4));
+    hold on
+end
+view(3)
---
+++ Final_project/initialSim.m
@@ -0,0 +1,9 @@
+%initial state
+x = 1;
+y = 2;
+z = 0;
+X = 1;
+Y = 1;
+Z = 6;
+step = 0.05;
+[PhiDesire,PhoDesire] = getAngle(x,y,z,X,Y,Z);
